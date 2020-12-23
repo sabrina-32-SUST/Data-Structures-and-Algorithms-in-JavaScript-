84c097051f3d4fde9b458569a9cf374eb6538a49
Sabrina <sanutaayvi@gmail.com>
2020-12-23 21:22:30 +0600

    QUEUE

--- JS/main.js
+++ JS/main.js
@@ -3,7 +3,7 @@
 
 var   letter  = [];
 
-var  word = "racecar";
+var  word = "dsfdfd";
 var  reverceword = "";
 
 for(  var  i=0;  i<word.length;  i++){
@@ -15,9 +15,68 @@ for(var  i =0;  i<word.length;  i++){
 }
 
 if(reverceword===word){
-    console.log(word+"is  a  palindrom");
+    console.log(word+" is  a  palindrom");
 }
 else{
-    console.log(word+"is not  a  palindrom");
+    console.log(word+" is not  a  palindrom");
 
 }
+
+ 
+//QUEUE
+
+function  Queue(){
+    collection = [];
+    this.print = function(){
+        console.log(collection);
+    };
+    this.enqueue = function(element){
+        collection.push(element);
+    };
+    this.dequeue =  function(){
+        return  collection.shift();
+    };
+    this.front =  function(){
+        return  collection[0];
+    };
+    this.size =  function(){
+        return  collection.length;
+    };
+    this.isEmpty =  function(){
+        return (collection.length===0);
+    };
+
+}
+
+
+
+var   queue = new  Queue();
+queue.enqueue("a");
+queue.enqueue("b");
+queue.enqueue("c");
+queue.enqueue("d");
+queue.print();
+queue.dequeue();
+queue.front();
+queue.print();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
